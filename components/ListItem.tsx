c353ff124ff7cb7d6d7986299eeb8934e9d3d373
herdeybayor <shereefdeen4real@gmail.com>
2022-10-28 18:56:56 +0100

    feat: add skeleton loading 🎉

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -24,7 +24,10 @@ function ListItem({}: Props) {
         "nft-collection"
     );
 
-    const ownedNfts = useOwnedNFTs(collectionContract, address);
+    const { data: nfts, isLoading: isLoadingNft } = useOwnedNFTs(
+        collectionContract,
+        address
+    );
 
     return (
         <main className="overflow-hidden">
@@ -37,24 +40,44 @@ function ListItem({}: Props) {
 
             <div className="p-4 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100">
                 <div className="flex items-center space-x-6">
-                    {ownedNfts.data?.map((nft) => (
-                        <div
-                            onClick={() => setSelectedNft(nft)}
-                            className="flex flex-col space-y-2 card min-w-fit border-2 bg-gray-100"
-                            key={nft.metadata.id}
-                        >
-                            <MediaRenderer
-                                className="h-36 rounded-lg"
-                                src={nft.metadata.image}
-                            />
-                            <p className="truncate text-lg font-bold">
-                                {nft.metadata.name}
-                            </p>
-                            <p className="truncate text-xs">
-                                {nft.metadata.description}
-                            </p>
-                        </div>
-                    ))}
+                    {isLoadingNft ? (
+                        <>
+                            {/* Skeleton Loading */}
+                            {Array(5)
+                                .fill(0)
+                                .map((_, i) => (
+                                    <div
+                                        key={i}
+                                        className="flex flex-col space-y-2 w-56 flex-shrink-0 border-2 p-5 rounded-lg animate-pulse"
+                                    >
+                                        <div className="h-36 w-full rounded-lg bg-gray-200 animate-pulse"></div>
+                                        <div className="bg-gray-200 h-2.5 rounded-full w-full animate-pulse"></div>
+                                        <div className="bg-gray-200 h-2 rounded-full w-full animate-pulse"></div>
+                                    </div>
+                                ))}
+                        </>
+                    ) : (
+                        <>
+                            {nfts?.map((nft) => (
+                                <div
+                                    onClick={() => setSelectedNft(nft)}
+                                    className="flex flex-col space-y-2 card min-w-fit border-2 bg-gray-100"
+                                    key={nft.metadata.id}
+                                >
+                                    <MediaRenderer
+                                        className="h-36 rounded-lg bg-gray-200"
+                                        src={nft.metadata.image}
+                                    />
+                                    <p className="truncate text-lg font-bold">
+                                        {nft.metadata.name}
+                                    </p>
+                                    <p className="truncate text-xs">
+                                        {nft.metadata.description}
+                                    </p>
+                                </div>
+                            ))}
+                        </>
+                    )}
                 </div>
             </div>
         </main>
