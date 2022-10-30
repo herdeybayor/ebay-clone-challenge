92e865c2fab23d567ae621edf97bfef8b3a664c8
herdeybayor <shereefdeen4real@gmail.com>
2022-10-30 11:06:27 +0100

    feat: add meta tags 🎉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,57 @@ function MyApp({ Component, pageProps }: AppProps) {
         <ThirdwebProvider desiredChainId={network}>
             <ThemeProvider enableSystem={true} attribute="class">
                 <Head>
+                    {/* <title>Alien Peace Marketplace</title>
+                    <link rel="icon" href="/favicon.ico" /> */}
+
                     <title>Alien Peace Marketplace</title>
-                    <link rel="icon" href="/favicon.ico" />
+                    <meta name="title" content="Alien Peace Marketplace" />
+                    <meta
+                        name="description"
+                        content="All Alien NFT in one place - ALIEN PEACE"
+                    />
+                    <link rel="icon" type="image/png" href="/favicon.ico" />
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:url"
+                        content="https://ebay-clone-challenge.vercel.app/"
+                    />
+                    <meta
+                        property="og:title"
+                        content="Alien Peace Marketplace"
+                    />
+                    <meta
+                        property="og:description"
+                        content="All Alien NFT in one place - ALIEN PEACE"
+                    />
+                    <meta
+                        property="og:image"
+                        content="https://ebay-clone-challenge.vercel.app/site-preview.png"
+                    />
+                    <meta
+                        property="og:updated_time"
+                        content="2021-12-06T09:23:21.056Z"
+                    />
+                    <meta
+                        property="twitter:card"
+                        content="summary_large_image"
+                    />
+                    <meta
+                        property="twitter:url"
+                        content="https://ebay-clone-challenge.vercel.app/"
+                    />
+                    <meta
+                        property="twitter:title"
+                        content="Alien Peace Marketplace"
+                    />
+                    <meta
+                        property="twitter:description"
+                        content="All Alien NFT in one place - ALIEN PEACE"
+                    />
+                    <meta
+                        property="twitter:image"
+                        content="https://ebay-clone-challenge.vercel.app/site-preview.png"
+                    />
                 </Head>
                 <Component {...pageProps} />
                 <Toaster position="top-center" reverseOrder={false} />
